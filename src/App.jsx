5f496e6e82f0ad6083ea7be885d8cd68a31e6365
Sergey Petrinich <petrinichshadow@gmail.com>
2022-08-30 19:16:32 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header, Footer } from "./_containers";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main className="page"></main>
+            <Footer />
+        </>
+    );
+};
+
+export default App;
---
+++ src/_containers/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container"></div>
+        </footer>
+    );
+};
+
+export default Footer;
